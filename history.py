27187a900863291b7ee074ca7a88ffd06c81a56f
Arjun Surya <arjunsurya2025@gmail.com>
2025-10-26 16:40:06 +0530

    Create history.py

---
+++ history.py
@@ -0,0 +1,51 @@
+styles = {
+    "conversationContent": {
+        "flex": 1,
+        "flexDirection": "row",
+        "alignItems": "center",
+    },
+    "iconContainer": {
+        "width": 48,
+        "height": 48,
+        "borderRadius": 24,
+        "backgroundColor": "#0f172a",
+        "justifyContent": "center",
+        "alignItems": "center",
+        "marginRight": 12,
+    },
+    "conversationInfo": {
+        "flex": 1,
+    },
+    "conversationTitle": {
+        "fontSize": 16,
+        "fontWeight": "600",
+        "color": "#fff",
+        "marginBottom": 4,
+    },
+    "conversationDate": {
+        "fontSize": 14,
+        "color": "#64748b",
+    },
+    "deleteButton": {
+        "padding": 8,
+        "marginLeft": 8,
+    },
+    "emptyContainer": {
+        "flex": 1,
+        "justifyContent": "center",
+        "alignItems": "center",
+        "paddingHorizontal": 32,
+    },
+    "emptyText": {
+        "fontSize": 18,
+        "fontWeight": "600",
+        "color": "#94a3b8",
+        "marginTop": 16,
+    },
+    "emptySubtext": {
+        "fontSize": 14,
+        "color": "#64748b",
+        "marginTop": 8,
+        "textAlign": "center",
+    },
+}
